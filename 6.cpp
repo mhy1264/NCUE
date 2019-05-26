a55e41d54a4f65508948bafee5345fba2aafd76b
mhy1264 <49542664+mhy1264@users.noreply.github.com>
2019-05-26 23:42:39 +0800

    mhy first commit

---
+++ 6.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	
+	int box[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&box[i]);
+		int a = box[i]*2;
+		int box[i][a];
+		for(j=0;j<a;j++)
+		{
+			scanf("%d",&box[i][a]);
+		}
+	}
+	
+	for(i=0;i<)
+
+
+
+
+
+
+
+
+ system("pause");
+}
+
+
